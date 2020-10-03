416e5fdeeaedaf8ae9413727d4264d1c81c2b47a
Jo Chong <jo.chong@mav3rik.com>
2020-10-03 22:06:00 +1000

    add signout component to app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { Provider } from 'react-redux';
 import store from './lib/redux';
 import InboxScreen from './components/InboxScreen';
 
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+
 function App() {
   return (
     <Provider store={store}>
       <InboxScreen />
+      <AmplifySignOut />
     </Provider>
   )
 }
 
-export default App;
+export default withAuthenticator(App);
